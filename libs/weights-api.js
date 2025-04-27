b2bfce7707a6c83511558832a81ec99cf99530b0
fox3000foxy <fox3000foxy@gmail.com>
2025-04-27 11:05:48 +0200

    chore: clean up code formatting and remove unused eslint directives in weights-api.js

--- libs/weights-api.js
+++ libs/weights-api.js
@@ -1,3 +1,7 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-this-alias */
+/* eslint-disable @typescript-eslint/no-require-imports */
+/* eslint-disable @typescript-eslint/no-unused-expressions */
 "use strict";
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
